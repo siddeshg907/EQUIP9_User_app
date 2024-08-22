ec0a041600450e121d8ade478337210eae18a82f
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2024-08-23 05:00:51 +0530

    updated index

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,33 @@
-const express=require("express")
-const {connection}=require("./db")
-const {userRouter}=require("./routes/user.routes")
-const cors=require("cors")
-require("dotenv").config()
+const express = require("express");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.routes");
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
 
-const path=require("path")
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+    res.status(200).send({ "msg": "This is Home Page" });
+});
 
+app.use("/api/users", userRouter);
 
-const __dirname=path.resolve()
-
-
-
-
-const app=express()
-app.use(express.json())
-
-app.use(cors())
-
-app.get("/",(req,res)=>{
-    res.status(200).send({"msg":"This is Hompe Page"})
-})
-
-app.use("/api/users",userRouter)
-
-
-
-app.listen(3000,async()=>{
+app.listen(3000, async () => {
     try {
-        await connection
-        console.log("Connected to the DB")
-        console.log(`server is running on 3000`)
+        await connection;
+        console.log("Connected to the DB");
+        console.log(`Server is running on port 3000`);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-    
-})
+});
 
-
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-  })
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
